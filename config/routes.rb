4585104bddce187e3d62558affa9682de0fd14e9
Eric <ericscottmarquez@gmail.com>
2019-11-03 14:20:51 -0700

    fixed errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :technicians
   resources :work_orders
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'work_orders#index'
